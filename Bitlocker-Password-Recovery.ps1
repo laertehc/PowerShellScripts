5b7fcae3abdb56dea02bf306678f5392b66256ca
laertehc <148168182+laertehc@users.noreply.github.com>
2023-11-13 21:36:40 -0300

    Add files via upload

---
+++ Bitlocker-Password-Recovery.ps1
@@ -0,0 +1,24 @@
+﻿clear
+$computer = @('')
+$computer = get-adcomputer -filter *|Sort-Object name
+$computerl = ''
+$Bitlocker = @()
+foreach($c in $computer){
+    $ad = Get-ADObject -Filter {ObjectClass -eq 'msFVE-RecoveryInformation'} -SearchBase $c.DistinguishedName -Properties "msFVE-RecoveryPassword", whencreated
+    #if($ad.Length -eq 0){write-host $c.Name -ForegroundColor red}
+    #else{ 
+        foreach($a in $ad){
+            $computername =  $c.Name
+            $password = $a."msFVE-RecoveryPassword"
+            $createddate = $a.whencreated
+            $Bitlocker+=[PSCustomObject]@{
+                ComputerName = $computername
+                PasswordRecovery  = $password
+                CreatedDate       = $createddate
+            }
+            
+        }
+    #}
+    
+}
+$Bitlocker|export-csv "C:\Report\BitlockerRecovery.csv" -NoTypeInformation
